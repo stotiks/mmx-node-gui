845aeb9e06f5917e754567e681721395868ecc5a
stotiks <47013208+stotiks@users.noreply.github.com>
2022-04-02 00:06:25 +0300

    init

---
+++ MMX_Node_GUI/MainForm.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace MMX_GUI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.chromiumWebBrowser1 = new CefSharp.WinForms.ChromiumWebBrowser();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // chromiumWebBrowser1
+            // 
+            this.chromiumWebBrowser1.ActivateBrowserOnCreation = false;
+            this.chromiumWebBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chromiumWebBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.chromiumWebBrowser1.Name = "chromiumWebBrowser1";
+            this.chromiumWebBrowser1.Size = new System.Drawing.Size(1008, 661);
+            this.chromiumWebBrowser1.TabIndex = 0;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 661);
+            this.Controls.Add(this.chromiumWebBrowser1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(1024, 700);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MMX Node";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CefSharp.WinForms.ChromiumWebBrowser chromiumWebBrowser1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ MMX_Node_GUI/MainForm.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MMX_GUI
+{
+
+    public partial class MainForm : Form
+    {
+        private const String url = "http://127.0.0.1:11380/gui/";
+
+        private Process process;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StartNode();
+            chromiumWebBrowser1.LoadUrl(url);
+        }
+
+        private void StartNode()
+        {
+            process = new Process();
+            process.StartInfo.WorkingDirectory = "C:\\Program Files\\MMX\\";
+            process.StartInfo.FileName = "C:\\Program Files\\MMX\\run_node.cmd";
+            process.StartInfo.UseShellExecute = false;
+            process.EnableRaisingEvents = true;
+            //process.OutputDataReceived += (sender1, args) => WriteTextSafe(args.Data);
+            process.Exited += new EventHandler(nodeProcess_HasExited);
+
+            process.OutputDataReceived += (sender1, args) => WriteProcessLog(args.Data);
+
+            if (false)
+            { 
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardError = true;
+            }
+
+            WriteProcessLog(String.Format("{0} {1}\r\n", process.StartInfo.FileName, process.StartInfo.Arguments));
+            process.Start();
+
+            if (process.StartInfo.RedirectStandardOutput) process.BeginOutputReadLine();
+        }
+
+        private void WriteProcessLog(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        private void nodeProcess_HasExited(object sender, EventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                notifyIcon1.Visible = true;
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon1.Visible = false;
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Do you want to close the application", System.Reflection.Assembly.GetExecutingAssembly().GetName().Name,
+                                            MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (dialogResult == DialogResult.No) e.Cancel = true;
+
+            if (ProcessExtensions.IsRunning(process))
+            {
+                process.CloseMainWindow();
+            }
+        }
+    }
+
+
+    public static class ProcessExtensions
+    {
+        public static bool IsRunning(this Process process)
+        {
+            if (process == null)
+            {
+                return false;
+            }
+
+            try
+            {
+                Process.GetProcessById(process.Id);
+            }
+            catch (ArgumentException)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
