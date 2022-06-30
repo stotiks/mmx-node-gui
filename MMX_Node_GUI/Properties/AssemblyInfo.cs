f0c58b0fa94597d70ae689aca9348f543b93edef
stotiks <47013208+stotiks@users.noreply.github.com>
2022-07-01 01:57:17 +0300

    MSBuilder.GenerateAssemblyInfo

--- MMX_Node_GUI/Properties/AssemblyInfo.cs
+++ MMX_Node_GUI/Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MMX Node GUI")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("MMX Node GUI")]
+//[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("madMAx43v3r")]
 [assembly: AssemblyProduct("MMX Node GUI")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("madMAx43v3r (C) Copyright 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+//[assembly: AssemblyVersion("0.6.0.0")]
+//[assembly: AssemblyFileVersion("0.6.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
