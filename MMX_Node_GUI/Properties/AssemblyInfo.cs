ed99fcedd7905a808c2e98c456910b93a823fe41
stotiks <47013208+stotiks@users.noreply.github.com>
2022-07-02 01:26:43 +0300

    AssemblyInfo.cs

--- MMX_Node_GUI/Properties/AssemblyInfo.cs
+++ MMX_Node_GUI/Properties/AssemblyInfo.cs
@@ -34,4 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 //[assembly: AssemblyVersion("0.6.0.0")]
 //[assembly: AssemblyFileVersion("0.6.0.0")]
-[assembly: NeutralResourcesLanguage("en")]
