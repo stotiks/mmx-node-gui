e38ddf7a6c944829d5e5b48c7af002deffb441b3
stotiks <47013208+stotiks@users.noreply.github.com>
2022-11-08 22:30:24 +0200

    StopProgramByAttachingToItsConsoleAndIssuingCtrlCEvent

--- Mmx.Gui.Win.Common/NativeMethods.cs
+++ Mmx.Gui.Win.Common/NativeMethods.cs
@@ -125,6 +125,50 @@ namespace Mmx.Gui.Win.Common
             }
         }
 
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool AttachConsole(uint dwProcessId);
+
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        static extern bool FreeConsole();
+
+        [DllImport("kernel32.dll")]
+        static extern bool SetConsoleCtrlHandler(ConsoleCtrlDelegate HandlerRoutine, bool Add);
+        // Delegate type to be used as the Handler Routine for SCCH
+        delegate Boolean ConsoleCtrlDelegate(CtrlTypes CtrlType);
+
+        // Enumerated type for the control messages sent to the handler routine
+        enum CtrlTypes : uint
+        {
+            CTRL_C_EVENT = 0,
+            CTRL_BREAK_EVENT,
+            CTRL_CLOSE_EVENT,
+            CTRL_LOGOFF_EVENT = 5,
+            CTRL_SHUTDOWN_EVENT
+        }
+
+        [DllImport("kernel32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool GenerateConsoleCtrlEvent(CtrlTypes dwCtrlEvent, uint dwProcessGroupId);
+
+        public static void StopProgramByAttachingToItsConsoleAndIssuingCtrlCEvent(Process proc)
+        {
+            //This does not require the console window to be visible.
+            if (AttachConsole((uint)proc.Id))
+            {
+                //Disable Ctrl-C handling for our program
+                SetConsoleCtrlHandler(null, true);
+                GenerateConsoleCtrlEvent(CtrlTypes.CTRL_C_EVENT, 0);
+
+                //Must wait here. If we don't and re-enable Ctrl-C handling below too fast, we might terminate ourselves.
+                //proc.WaitForExit();
+
+                FreeConsole();
+
+                //Re-enable Ctrl-C handling or any subsequently started programs will inherit the disabled state.
+                SetConsoleCtrlHandler(null, false);
+            }
+        }
+
     }
     
 }
