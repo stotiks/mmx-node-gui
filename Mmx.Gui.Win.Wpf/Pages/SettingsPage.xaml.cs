bac4d86584a57c918acc2f15f46b108ca8f14512
stotiks <47013208+stotiks@users.noreply.github.com>
2022-11-14 22:42:16 +0200

    DebugGroupBox enable on 3-click

--- Mmx.Gui.Win.Wpf/Pages/SettingsPage.xaml.cs
+++ Mmx.Gui.Win.Wpf/Pages/SettingsPage.xaml.cs
@@ -2,13 +2,15 @@
 using System.Linq;
 using System.Collections.Generic;
 using System.Windows.Controls;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace Mmx.Gui.Win.Wpf.Pages
 {
     /// <summary>
     /// Interaction logic for SettingsPage.xaml
     /// </summary>
-    public partial class SettingsPage : Page
+    public partial class SettingsPage : Page, INotifyPropertyChanged
     {
 
         private Dictionary<string, string> _launguages = new Dictionary<string, string>(){
@@ -23,7 +25,8 @@ namespace Mmx.Gui.Win.Wpf.Pages
             { "zh", "简体中文" },
         };
 
-        public Dictionary<string, string> Languages { 
+        public Dictionary<string, string> Languages
+        {
             get => _launguages;
         }
 
@@ -37,7 +40,7 @@ namespace Mmx.Gui.Win.Wpf.Pages
         {
             get => _updateIntervals;
         }
-        
+
 
         public Array Themes
         {
@@ -51,6 +54,36 @@ namespace Mmx.Gui.Win.Wpf.Pages
         {
             InitializeComponent();
             DataContext = this;
+        }
+
+        private bool _debugGroupBoxIsEnabled = false;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public bool DebugGroupBoxIsEnabled
+        {
+            get => _debugGroupBoxIsEnabled;
+
+            private set {
+                _debugGroupBoxIsEnabled = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private void DebugGroupBox_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.ClickCount >= 3)
+            {
+                DebugGroupBoxIsEnabled = !DebugGroupBoxIsEnabled;
+            }
         }
 
     }
