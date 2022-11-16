cb9a4882bcd7ca1ff23b49f8be1284439f4d4436
stotiks <47013208+stotiks@users.noreply.github.com>
2022-11-16 19:50:54 +0200

    harvester nav menu

--- Mmx.Gui.Win.Wpf.Harvester/MainWindow.xaml.cs
+++ Mmx.Gui.Win.Wpf.Harvester/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Mmx.Gui.Win.Wpf.Common;
 using Mmx.Gui.Win.Wpf.Common.Pages;
+using ModernWpf.Controls;
 
 namespace Mmx.Gui.Win.Wpf.Harvester
 {
@@ -14,5 +15,22 @@ namespace Mmx.Gui.Win.Wpf.Harvester
             InitializeComponent();
             contentFrame.Content = harvesterPage;
         }
+
+        private void NavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            var selectedItem = (NavigationViewItem)args.SelectedItem;
+
+            switch (selectedItem.Tag)
+            {
+                case "ConnectPage":
+                    contentFrame.Content = null;
+                    break;
+                case "HarvesterPage":
+                    contentFrame.Content = harvesterPage;
+                    break;
+            }
+
+        }
+
     }
 }
