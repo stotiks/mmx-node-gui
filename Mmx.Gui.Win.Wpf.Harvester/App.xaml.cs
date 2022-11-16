9b23e5e1eba969833f35d6ace9e942ca157b9150
stotiks <47013208+stotiks@users.noreply.github.com>
2022-11-16 19:46:33 +0200

    harvester init

---
+++ Mmx.Gui.Win.Wpf.Harvester/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Mmx.Gui.Win.Common;
+using Mmx.Gui.Win.Common.Properties;
+using System;
+using System.Globalization;
+using System.Threading;
+using System.Windows;
+
+namespace Mmx.Gui.Win.Wpf.Harvester
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo(Settings.Default.LanguageCode);
+            Thread.CurrentThread.CurrentCulture = new CultureInfo(Settings.Default.LanguageCode);
+
+            if (!SingleInstance.IsFirstInstance())
+            {
+                if (!SingleInstance.ShowFirstInstance())
+                {
+                    MessageBox.Show(Common.Properties.Resources.Another_Instance_Running, Common.Properties.Resources.Warning);
+                }
+                Application.Current.Shutdown();
+            }
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show((e.ExceptionObject as Exception).ToString(), "Warning! UnhandledException");
+        }
+    }
+}
---
+++ Mmx.Gui.Win.Wpf.Harvester/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Mmx.Gui.Win.Wpf.Common;
+using Mmx.Gui.Win.Wpf.Common.Pages;
+
+namespace Mmx.Gui.Win.Wpf.Harvester
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : WpfMainWindow
+    {
+        private HarvesterPage harvesterPage = new HarvesterPage();
+        public MainWindow()
+        {
+            InitializeComponent();
+            contentFrame.Content = harvesterPage;
+        }
+    }
+}
