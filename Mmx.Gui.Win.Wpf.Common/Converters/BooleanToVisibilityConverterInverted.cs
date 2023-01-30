d38182faaeae61cd0ed3f73c8af987a673195e20
stotiks <47013208+stotiks@users.noreply.github.com>
2023-01-31 01:39:40 +0200

    connection page

---
+++ Mmx.Gui.Win.Wpf.Common/Converters/BooleanToVisibilityConverterInverted.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Mmx.Gui.Win.Wpf.Common.Converters
+{
+    [ValueConversion(typeof(bool), typeof(Visibility))]
+    public class BooleanToVisibilityConverterInverted : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool bValue = false;
+            if (value is bool b)
+            {
+                bValue = b;
+            }
+            else if (value is Nullable<bool>)
+            {
+                Nullable<bool> tmp = (Nullable<bool>)value;
+                bValue = tmp.HasValue ? tmp.Value : false;
+            }
+            bValue = (parameter != null) ? !bValue : bValue;
+            return (bValue) ? Visibility.Hidden : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+            //if (value is Visibility)
+            //{
+            //    return (Visibility)value == Visibility.Visible;
+            //}
+            //else
+            //{
+            //    return false;
+            //}
+        }
+    }
+}
--- Mmx.Gui.Win.Wpf.Harvester/Pages/ConnectionPage.xaml.cs
+++ Mmx.Gui.Win.Wpf.Harvester/Pages/ConnectionPage.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using Mmx.Gui.Win.Common;
 using Mmx.Gui.Win.Common.Harvester;
 using System;
+using System.ComponentModel;
 using System.Net;
+using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -10,7 +12,7 @@ namespace Mmx.Gui.Win.Wpf.Harvester.Pages
     /// <summary>
     /// Interaction logic for ConnectionPage.xaml
     /// </summary>
-    public partial class ConnectionPage
+    public partial class ConnectionPage : INotifyPropertyChanged
     {
         public HarvesterOptions HarvesterOptions => HarvesterOptions.Instance;
         private HarvesterProcess _harvesterProcess;
@@ -26,17 +28,95 @@ namespace Mmx.Gui.Win.Wpf.Harvester.Pages
             _harvesterProcess = harvesterProcess;
             _harvesterProcess.OutputDataReceived += _logger.OutputDataReceived;
             _harvesterProcess.ErrorDataReceived += _logger.ErrorDataReceived;
+
+            PropertyChanged += (o, e) =>
+            {
+                if(e.PropertyName == nameof(DetectRunning))
+                {
+                    ConnectElementsEnabled = !DetectRunning;
+                    //ConnectButtonEnabled = ConnectElementsEnabled;
+                }
+
+                if(e.PropertyName == nameof(ConnectElementsEnabled))
+                {
+                    ConnectButtonEnabled = !HarvesterProcess.IsRunning && ConnectElementsEnabled;
+                }
+
+            };
+
+            HarvesterProcess.PropertyChanged += (o, e) =>
+            {
+                if (e.PropertyName == nameof(HarvesterProcess.IsRunning))
+                {
+                    ConnectElementsEnabled = !HarvesterProcess.IsRunning;
+                    ConnectButtonVisibility = ConnectElementsEnabled ? Visibility.Visible : Visibility.Hidden;
+                }
+            };
+            
         }
+
+        private bool _detectRunning;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public bool DetectRunning
+        {
+            get => _detectRunning;
+
+            private set
+            {
+                _detectRunning = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private bool _connectElementsEnabled = true;
+        public bool ConnectElementsEnabled
+        {
+            get => _connectElementsEnabled;
+
+            private set
+            {
+                _connectElementsEnabled = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private bool _connectButtonEnabled = true;
+        public bool ConnectButtonEnabled
+        {
+            get => _connectButtonEnabled;
+
+            private set
+            {
+                _connectButtonEnabled = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private Visibility _connectButtonVisibility = Visibility.Visible;
+        public Visibility ConnectButtonVisibility
+        {
+            get => _connectButtonVisibility;
+
+            private set
+            {
+                _connectButtonVisibility = value;
+                NotifyPropertyChanged();
+            }
+        }
+        
 
         private void DetectButton_Click(object sender, RoutedEventArgs e)
         {
-            ConnectionGroup.IsEnabled = false;
+            DetectRunning = true;
             HarvesterOptions.DetectNodeIP().ContinueWith(task =>
             {
-                Dispatcher.BeginInvoke(new Action(delegate
-                {
-                    ConnectionGroup.IsEnabled = true;
-                }));
+                DetectRunning = false;
             });
         }
 
@@ -50,6 +130,11 @@ namespace Mmx.Gui.Win.Wpf.Harvester.Pages
         private void TextBoxLog_TextChanged(object sender, TextChangedEventArgs e)
         {
             (sender as TextBox).ScrollToEnd();
+        }
+
+        private void DisconnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            _harvesterProcess.Stop();
         }
     }
 }
