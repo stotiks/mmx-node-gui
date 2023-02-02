14d82fb625fe85c9676a462a6a9ecf04156044a1
stotiks <47013208+stotiks@users.noreply.github.com>
2023-02-02 16:41:51 +0200

    InvertedBooleanToVisibilityConverter rename

--- Mmx.Gui.Win.Wpf.Common/Converters/InvertedBooleanToVisibilityConverter.cs
+++ Mmx.Gui.Win.Wpf.Common/Converters/InvertedBooleanToVisibilityConverter.cs
@@ -6,7 +6,7 @@ using System.Windows.Data;
 namespace Mmx.Gui.Win.Wpf.Common.Converters
 {
     [ValueConversion(typeof(bool), typeof(Visibility))]
-    public class BooleanToVisibilityConverterInverted : IValueConverter
+    public class InvertedBooleanToVisibilityConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
