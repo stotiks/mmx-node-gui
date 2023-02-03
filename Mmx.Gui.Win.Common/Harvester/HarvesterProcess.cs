ba078478abc61a09fffb98abfdaa997fc9958a93
stotiks <47013208+stotiks@users.noreply.github.com>
2023-02-03 04:54:53 +0200

    clean up

--- Mmx.Gui.Win.Common/Harvester/HarvesterProcess.cs
+++ Mmx.Gui.Win.Common/Harvester/HarvesterProcess.cs
@@ -25,11 +25,5 @@ namespace Mmx.Gui.Win.Common.Harvester
 
             Start(processStartInfo);
         }
-
-        //public override void Stop()
-        //{
-        //    Kill();
-        //    KillByProcessName("mmx_harvester");
-        //}
     }
 }
