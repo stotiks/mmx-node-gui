933c2e157632739a10bca66b2ee7a5f02241978a
stotiks <47013208+stotiks@users.noreply.github.com>
2023-02-11 00:02:55 +0200

    gh init

---
+++ Mmx.Gui.Win.Wpf.Plotter.Chia/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel;
+using Mmx.Gui.Win.Wpf.Common.Pages;
+using ModernWpf.Controls;
+using System.Threading.Tasks;
+
+namespace Mmx.Gui.Win.Wpf.Plotter.Chia
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private readonly PlotterPage _plotterPage = new PlotterPage();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ContentFrame.Content = _plotterPage;
+
+            Closing += MainWindow_Closing;
+        }
+
+        private async Task MainWindow_Closing(object sender, CancelEventArgs args)
+        {
+            await Task.Yield();
+
+            if (_plotterPage.PlotterDialog.PlotterProcess.IsRunning)
+            {
+                var dialog = new ContentDialog
+                {
+                    Title = "Stop plotter before exit!", //TODO i18n
+                    PrimaryButtonText = "OK", //TODO i18n
+                    DefaultButton = ContentDialogButton.Primary
+                };
+
+                await dialog.ShowAsync();
+
+                dialog.Hide();
+                args.Cancel = true;
+            }
+        }
+    }
+}
