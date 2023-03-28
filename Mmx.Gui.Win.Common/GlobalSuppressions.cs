e8418662c2588edd2dcc36c553709597b1121d3f
stotiks <47013208+stotiks@users.noreply.github.com>
2023-03-28 17:47:16 +0300

    GlobalSuppressions

---
+++ Mmx.Gui.Win.Common/GlobalSuppressions.cs
@@ -0,0 +1,34 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~P:Mmx.Gui.Win.Common.Plotter.PlotterOptions.count")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~P:Mmx.Gui.Win.Common.Plotter.PlotterOptions.buckets")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~P:Mmx.Gui.Win.Common.Plotter.PlotterOptions.buckets3")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~P:Mmx.Gui.Win.Common.Plotter.PlotterOptions.contract")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~P:Mmx.Gui.Win.Common.Plotter.PlotterOptions.device")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~P:Mmx.Gui.Win.Common.Plotter.PlotterOptions.directout")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~P:Mmx.Gui.Win.Common.Plotter.PlotterOptions.farmerkey")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~P:Mmx.Gui.Win.Common.Plotter.PlotterOptions.finaldir")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~P:Mmx.Gui.Win.Common.Plotter.PlotterOptions.level")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~P:Mmx.Gui.Win.Common.Plotter.PlotterOptions.memory")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~P:Mmx.Gui.Win.Common.Plotter.PlotterOptions.multifinaldir")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~P:Mmx.Gui.Win.Common.Plotter.PlotterOptions.ndevices")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~P:Mmx.Gui.Win.Common.Plotter.PlotterOptions.nftplot")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~P:Mmx.Gui.Win.Common.Plotter.PlotterOptions.pin_memory")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~P:Mmx.Gui.Win.Common.Plotter.PlotterOptions.plotter")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~P:Mmx.Gui.Win.Common.Plotter.PlotterOptions.poolkey")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~P:Mmx.Gui.Win.Common.Plotter.PlotterOptions.port")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~P:Mmx.Gui.Win.Common.Plotter.PlotterOptions.priority")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~P:Mmx.Gui.Win.Common.Plotter.PlotterOptions.rmulti2")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~P:Mmx.Gui.Win.Common.Plotter.PlotterOptions.size")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~P:Mmx.Gui.Win.Common.Plotter.PlotterOptions.stagedir")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~P:Mmx.Gui.Win.Common.Plotter.PlotterOptions.streams")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~P:Mmx.Gui.Win.Common.Plotter.PlotterOptions.threads")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~P:Mmx.Gui.Win.Common.Plotter.PlotterOptions.tmpdir")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~P:Mmx.Gui.Win.Common.Plotter.PlotterOptions.tmpdir2")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~P:Mmx.Gui.Win.Common.Plotter.PlotterOptions.tmpdir3")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~P:Mmx.Gui.Win.Common.Plotter.PlotterOptions.tmptoggle")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~P:Mmx.Gui.Win.Common.Plotter.PlotterOptions.waitforcopy")]
