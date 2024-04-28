81110d98ba38fb669a51f841bc5638cc6d9f44a9
stotiks <47013208+stotiks@users.noreply.github.com>
2024-04-28 06:47:55 +0300

    split

--- Mmx.Gui.Win.Common/Plotter/Item.cs
+++ Mmx.Gui.Win.Common/Plotter/Item.cs
@@ -2,8 +2,6 @@
 using System;
 using System.Collections;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
 
 namespace Mmx.Gui.Win.Common.Plotter
 {
@@ -87,36 +85,6 @@ namespace Mmx.Gui.Win.Common.Plotter
         {
             return Value ? base.GetParam() : "";
         }
-    }
-
-    public class ItemBase<T>: INotifyPropertyChanged
-    {
-        public event PropertyChangedEventHandler PropertyChanged;
-        protected void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-        protected bool _valueInitialized;
-        protected T _value;
-
-        public string Name { get; set; }
-        public T Value
-        {
-            get => _value;
-            set
-            {
-                _valueInitialized = true;
-                if (!_value.Equals(value))
-                {
-                    _value = value;
-                    NotifyPropertyChanged();
-                }
-                
-            }
-        }
-
-        public virtual JToken JValue => new JValue(Value);
     }
 
     public abstract class Item<T> : ItemBase<T>
---
+++ Mmx.Gui.Win.Common/Plotter/ItemBase.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json.Linq;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Mmx.Gui.Win.Common.Plotter
+{
+    public class ItemBase<T>: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool _valueInitialized;
+        protected T _value;
+
+        public string Name { get; set; }
+        public T Value
+        {
+            get => _value;
+            set
+            {
+                _valueInitialized = true;
+                if (!_value.Equals(value))
+                {
+                    _value = value;
+                    NotifyPropertyChanged();
+                }
+                
+            }
+        }
+
+        public virtual JToken JValue => new JValue(Value);
+    }
+
+}
